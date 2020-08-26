516fe040f9a31275648a3d726573e90314511001
duboc <andersonduboc@gmail.com>
2020-08-26 16:52:37 -0300

    oci

--- terraform/oci/variables.tf
+++ terraform/oci/variables.tf
@@ -1,6 +1,6 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key" {}
 variable "compartment_ocid" {}
 variable "region" {}
