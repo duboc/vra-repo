72e18ec8e34a84105b91f47fc6670828165ad939
duboc <andersonduboc@gmail.com>
2020-08-26 19:33:09 -0300

    agora com valdeco

--- oracle-tf/variables.tf
+++ oracle-tf/variables.tf
@@ -1,6 +1,6 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private-key" {}
+variable "private_key" {}
 variable "compartment_ocid" {}
 variable "region" {}
