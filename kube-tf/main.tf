d76921014688217b475258da5332a0a171b015eb
duboc <andersonduboc@gmail.com>
2020-09-06 09:43:02 -0300

    namespace

--- kube-tf/main.tf
+++ kube-tf/main.tf
@@ -6,25 +6,9 @@ provider "kubernetes" {
 }
 
 
-resource "kubernetes_pod" "nginx" {
+resource "kubernetes_namespace" "meuSpace" {
     metadata {
-        name = "nginx-example"
-        labels = {
-            App = "nginx"
-        } 
-    }
-
-    spec {
-        container {
-            image = "nginx:1.7.8"
-            name = "example"
-            
-            port {
-                container_port = 80
-            } 
-        } 
-    }
+        name = "meuNamespace"
+    } 
 } 
-
-
 
