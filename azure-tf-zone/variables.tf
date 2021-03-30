2f182d478208b7f7149310a9c2e971c1d567e458
Anderson Duboc <andersonduboc@gmail.com>
2021-03-29 23:00:13 -0300

    new azure zone

---
+++ azure-tf-zone/variables.tf
@@ -0,0 +1,13 @@
+variable "prefix" {
+  default = "duboc"
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "zona" {
+  default = 1
+} 
+
+variable "location" {
+  default = "Brazil South"
+  description = "The Azure Region in which all resources in this example should be created."
+}
