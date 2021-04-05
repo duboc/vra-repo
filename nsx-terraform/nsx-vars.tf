e7c0033b68473a9087e192c9aa713e876e068131
Anderson Duboc <aduboc@vmware.com>
2021-04-05 16:30:47 -0300

    nsx4

--- nsx-terraform/nsx-vars.tf
+++ nsx-terraform/nsx-vars.tf
@@ -8,3 +8,21 @@ provider "nsxt" {
   retry_max_delay       = 5000
   retry_on_status_codes = [429]
 }
+
+resource "nsxt_policy_segment" "segment1" {
+    connectivity_path   = "/infra/tier-0s/t0"
+    display_name        = var.meuportgroup
+    transport_zone_path = "/infra/sites/default/enforcement-points/default/transport-zones/1b3a2f36-bfd1-443e-a0f6-4de01abc963e"
+    vlan_ids            = []
+
+    advanced_config {
+        connectivity = "ON"
+        hybrid       = false
+        local_egress = false
+    }
+
+    subnet {
+        cidr        = "172.16.60.1/24"
+    }
+}
+
