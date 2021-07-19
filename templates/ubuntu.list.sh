6005947ab499c8c61e51bde3ee09f0b386820d42
Anderson Duboc <aduboc@vmware.com>
2021-07-19 09:52:22 -0300

    new ub

--- templates/ubuntu.list.sh
+++ templates/ubuntu.list.sh
@@ -1,2 +1,3 @@
 wget http://releases.ubuntu.com/18.04.3/ubuntu-18.04.3-live-server-amd64.iso
 wget https://cloud-images.ubuntu.com/releases/bionic/release/ubuntu-18.04-server-cloudimg-amd64.ova
+wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.ova
