d5da58b9b9be8c2c9cc609ec714d2b665a746d1d
Anderson Duboc <aduboc@vmware.com>
2021-10-04 17:17:50 -0300

    eks

---
+++ gcp-tf/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = jsondecode(var.google_creds)
+  
+  project = "sddc-latam"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
